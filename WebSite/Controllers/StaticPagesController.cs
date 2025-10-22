d7afbfd2b66335978e4887cd6e6d0815899e076b
vusalhasanov99 <94562855+vusalhasanov99@users.noreply.github.com>
2025-10-22 17:35:24 +0400

    aventroclub

---
+++ WebSite/Controllers/StaticPagesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class StaticPagesController : Controller
+    {
+        [Route("privacy/index.php")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("cookie/index.php")]
+        public IActionResult Cookie()
+        {
+            return View();
+        }
+
+        [Route("terms/index.php")]
+        public IActionResult Terms()
+        {
+            return View();
+        }
+
+        [Route("about/index.php")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("allgame/index.php")]
+        public IActionResult Allgame()
+        {
+            return View();
+        }
+
+        [Route("contact/index.php")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+   
+    }
+}
---
+++ WebSite/wwwroot/game.js
@@ -0,0 +1,300 @@
+﻿// prefix after cutt.ly/
+const games = [
+
+
+    {
+        title: "Ice Lobster",
+        source: "https://demogamesfree.pragmaticplay.net/gs2c/openGame.do?gameSymbol=vs20stickypos&websiteUrl=https%3A%2F%2Fdemogamesfree.pragmaticplay.net&jurisdiction=99&lobby_url=https%3A%2F%2Fwww.pragmaticplay.com%2Fen%2F&lang=EN&cur=USD",
+        prefix: "9eRJluzg",
+        img: "ice.jpg",
+        rating: 4,
+    },
+
+    {
+        title: "Heroic Spins",
+        source: "https://demogamesfree.pragmaticplay.net/gs2c/openGame.do?gameSymbol=vs20shootstars&websiteUrl=https%3A%2F%2Fdemogamesfree.pragmaticplay.net&jurisdiction=99&lobby_url=https%3A%2F%2Fwww.pragmaticplay.com%2Fen%2F&lang=EN&cur=USD",
+        prefix: "0eIUfCut",
+        img: "heroic-spins-tile.jpg",
+        rating: 4,
+    },
+
+
+
+    {
+        title: "Heart of Cleopatra",
+        source: "https://demogamesfree.pragmaticplay.net/gs2c/openGame.do?gameSymbol=vs20heartcleo&websiteUrl=https%3A%2F%2Fdemogamesfree.pragmaticplay.net&jurisdiction=99&lobby_url=https%3A%2F%2Fwww.pragmaticplay.com%2Fen%2F&lang=EN&cur=USD",
+        prefix: "BeIUjaot",
+        img: "32713_4.jpg",
+        rating: 4,
+    },
+
+    {
+        title: "Aztec Powernudge",
+        source: "https://demogamesfree.pragmaticplay.net/gs2c/openGame.do?gameSymbol=vs20sbpnudge&websiteUrl=https%3A%2F%2Fdemogamesfree.pragmaticplay.net&jurisdiction=99&lobby_url=https%3A%2F%2Fwww.pragmaticplay.com%2Fen%2F&lang=EN&cur=USD",
+        prefix: "DeIUc5kQ",
+        img: "aztec.jpg",
+        rating: 4,
+    },
+    {
+        title: "Ripe Rewards",
+        source: "https://demogamesfree.pragmaticplay.net/gs2c/openGame.do?gameSymbol=vs40stckwldlvl&websiteUrl=https%3A%2F%2Fdemogamesfree.pragmaticplay.net&jurisdiction=99&lobby_url=https%3A%2F%2Fwww.pragmaticplay.com%2Fen%2F&lang=EN&cur=USD",
+        prefix: "DeIUn9F0",
+        img: "rewards.jpg",
+        rating: 4,
+    },
+
+    {
+        title: "Book of Ra Deluxe",
+        source: "https://nrgs-b2b.greentube.com/Nrgs/B2B/Adapter/UnibetIframe/v1/games/launch/game?channel=Desktop&gameID=110&useRealmoney=false&lang=en&region=MT&placeholderId=game-container-id",
+        prefix: "7w7OtKKg",
+        img: "book-of-ra-deluxe.png",
+        rating: 4,
+    },
+
+    {
+        title: "Triple Diamond",
+        source: "https://platform.ka.rgsgames.com/skb/gateway?skincode=AG01&countrycode=US&nscode=AGLC&language=en&technology=HTML&currencycode=FPY&softwareid=200-1221-001&presenttype=STD",
+        prefix: "PeujD6VO",
+        img: "triple-diamond.jpg",
+        rating: 5,
+    },
+
+
+
+    {
+        title: "More Magic Apple",
+        source: "https://3oaks.com/api/v1/games/more_magic_apple/play?lang=en",
+        prefix: "4eukQWWx",
+        img: "More-Magic-Apple.jpg",
+        rating: 3,
+    },
+    {
+        title: "Mega Wild Fruits",
+        source: "https://cdn-live.spinomenal.com/external_components/generic-play.html?partnerId=SPIN-DEBUG&launchToken=e697263342dc6a328946&gameCode=SlotMachine_MegaWildFruits&langCode=en_US&IsFunMode=true&inter=0&InitClientUrl=https%3a%2f%2frgs-ld-demo.spinomenal.com%2fapi%2fInitClientUrl",
+        prefix: "7eukW4F7",
+        img: "mega-wild-fruits.jpg",
+        rating: 4,
+    },
+    {
+        title: "Solar Odyssey",
+        source: "https://cdn-live.spinomenal.com/external_components/generic-play.html?partnerId=SPIN-DEBUG&launchToken=e962cb034228ef328941&gameCode=SlotMachine_SolarOdyssey&langCode=en_US&IsFunMode=true&inter=0&InitClientUrl=https%3a%2f%2frgs-ld-demo.spinomenal.com%2fapi%2fInitClientUrl",
+        prefix: "MeukTyBi",
+        img: "solar-odyssey.jpg",
+        rating: 4,
+    },
+    {
+        title: "Burning Chilli X",
+        source: "https://demo.bgaming-network.com/games/BurningChilliX/FUN?play_token=33dc831c-2983-4c69-b9eb-f04a6f70018e",
+        prefix: "1eukY6N6",
+        img: "Burning-Chilli-X.jpg",
+        rating: 5,
+    },
+    {
+        title: "Mighty Wild",
+        source: "https://gamelaunch.wazdan.com/demo-demo/gamelauncher?license=wazdancom&mode=demo&game=446&lang=en",
+        prefix: "HeukI23P",
+        img: "mighty-wild-panther.jpg",
+        rating: 4,
+    },
+    {
+        title: "Big Bamboo",
+        source: "https://player.eu.regulated.pushgaming.com/hive/b2c/game/bigbamboo-01/client/index.php?igpCode=dm-verajohn&guest=true&mode=demo&lang=sv&country=EN&meshSessionId=9b5ece59-1e8b-4b6e-8af4-6417600ce675&ccyCode=EUR&jurisdiction=MT",
+        prefix: "VeukADOt",
+        img: "Big-Bamboo.jpg",
+        rating: 5,
+    },
+    {
+        title: "Bonanza Billion",
+        source: "https://demo.bgaming-network.com/games/BonanzaBillion/FUN?play_token=e94f302b-eee6-46ad-85ab-f6011b014132",
+        prefix: "EeukDR0z",
+        img: "Bonanza-Billion.jpg",
+        rating: 4,
+    },
+    {
+        title: "40 Sweet Fruits",
+        source: "https://cdn-v2.gamzix.com/40-sweet-fruits/v2.0.9/?gid=1043&host=sapp.gamzix.com&port=443&client=&lang=en&home=&sid=avklbqdzm&wss=1&promoVersion=v2.0.8&slot_core=v0.0.21f",
+        prefix: "8eukKjk9",
+        img: "40-Sweet-Fruits.jpg",
+        rating: 3,
+    },
+    {
+        title: "Sun of Egypt 3",
+        source: "https://3oaks.com/api/v1/games/sun_of_egypt_3/play?lang=en",
+        prefix: "XeukLCgW",
+        img: "Sun-of-Egypt-3.jpg",
+        rating: 3,
+    },
+    {
+        title: "Sevens on Fire",
+        source: "https://demo.championstudio.net/?p=%7B%22game_id%22%3A%201002%2C%20%22login_code%22%3A%20%22%5B%5B00000000000000%5D%5D%22%2C%20%22game_exit%22%3A%20%22%5B%5Bclose%5D%5D%22%2C%20%22back_url%22%3A%20%22https%3A//10.7.16.13/game/2/%22%7D",
+        prefix: "JeukCH3n",
+        img: "Sevens-on-Fire.jpg",
+        rating: 3,
+    },
+    {
+        title: "30 Coins",
+        source: "https://gamelaunch.voltent.com/488ovo1w/gamelauncher?operator=livebet&game=444&lang=en&mode=demo&platform=desktop&lobbyUrl=https://slotcatalog.com/en",
+        prefix: "CeukBujh",
+        img: "30-coins.jpg",
+        rating: 4,
+    },
+    {
+        title: "Big Banker Bonanza",
+        source: "https://netgamenv.com/netgame_games/db.php?game=big-banker-bonanza",
+        prefix: "6eukN8la",
+        img: "big-banker-bonanza.jpg",
+        rating: 5,
+    },
+    {
+        title: "Leshy's Magical Forest",
+        source: "https://cdn-live.spinomenal.com/external_components/generic-play.html?partnerId=SPIN-DEBUG&launchToken=AFF-DEMO&gameCode=SlotMachine_LeshysMagicalForest&langCode=en_EN&HomeUrl=https%3a%2f%2fwww.spinomenal.com&IsFunMode=true&inter=0&InitClientUrl=https%3a%2f%2frgs-ld-demo.spinomenal.com%2fapi%2fInitClientUrl",
+        prefix: "Weuk0PgZ",
+        img: "leshy-s-magical-forest.jpg",
+        rating: 3,
+    },
+    {
+        title: "Chicken Rush",
+        source: "https://demo.bgaming-network.com/games/ChickenRush/FUN?play_token=d4d5e291-87d5-4604-8367-5a297ee828fa",
+        prefix: "Meuk9i3e",
+        img: "chicken-rush.jpg",
+        rating: 5,
+    },
+    {
+        title: "Book of Pinata",
+        source: "https://book-of-pinata.onlyplaygames.net/?sid=2006223b951b95d6fa913a64aa57185963df3379b6aba4640397dde7feac9ded1594018c1d4ae00c088c399c6efcc1eefa1ee740a94eaf60d27801cc93fd2c8e&gid=47104687&api=api-eu.ig-onlyplay.net/api&pid=1&launchedForPid=1&params=eyJzaWQiOiIyMDA2MjIzYjk1MWI5NWQ2ZmE5MTNhNjRhYTU3MTg1OTYzZGYzMzc5YjZhYmE0NjQwMzk3ZGRlN2ZlYWM5ZGVkMTU5NDAxOGMxZDRhZTAwYzA4OGMzOTljNmVmY2MxZWVmYTFlZTc0MGE5NGVhZjYwZDI3ODAxY2M5M2ZkMmM4ZSIsImdpZCI6IjQ3MTA0Njg3IiwiYXBpIjoiYXBpLWV1LmlnLW9ubHlwbGF5Lm5ldFwvYXBpIiwicGlkIjoiMSIsImxhdW5jaGVkRm9yUGlkIjoiMSJ9",
+        prefix: "Xeuk8wJb",
+        img: "book-of-pinata.jpg",
+        rating: 4,
+    },
+    {
+        title: "Adventures",
+        source: "https://demo.bgaming-network.com/games/Adventures/FUN?play_token=b8a312b1-4fa4-4ac0-abb4-c862cd2272f0",
+        prefix: "neulfq3a",
+        img: "adventures.jpg",
+        rating: 5,
+    },
+    {
+        title: "Midas Golden Touch 2",
+        source: "https://forfun.thunderkick.com/gamelauncher/play/generic?device=desktop&gameId=tk-s1-g74-94&operatorId=2&playMode=demo",
+        prefix: "2eulg8fL",
+        img: "midas-golden-touch-2.jpg",
+        rating: 3,
+    },
+    {
+        title: "Book of Dead",
+        source: "https://asccw.playngonetwork.com/Casino/IframedView?pid=2&gid=bookofdead&lang=en_US&practice=1&channel=desktop&div=flashobject&width=100%25&height=100%25&user=&password=&ctx=&demo=2&brand=&lobby=&rccurrentsessiontime=0&rcintervaltime=0&rcaccounthistoryurl=&rccontinueurl=&rcexiturl=&rchistoryurlmode=&autoplaylimits=0&autoplayreset=0&callback=flashCallback&rcmga=&resourcelevel=0&hasjackpots=False&country=&pauseplay=&playlimit=&selftest=&sessiontime=&coreweburl=https://asccw.playngonetwork.com/&showpoweredby=True",
+        prefix: "feuljINN",
+        img: "Book-of-Dead.jpg",
+        rating: 4,
+    },
+    {
+        title: "Money Ways",
+        source: "https://cu.thunderspin.net/games/moneyways/",
+        prefix: "9eullUbI",
+        img: "money-ways.jpg",
+        rating: 4,
+    },
+    {
+        title: "Majestic Wild Buffalo",
+        source: "https://cdn-live.spinomenal.com/external_components/generic-play.html?partnerId=SPIN-DEBUG&launchToken=AFF-DEMO&gameCode=SlotMachine_MajesticWildBuffalo-DTD&langCode=en_EN&HomeUrl=https://slotcatalog.com/en&IsFunMode=true&inter=0&InitClientUrl=https%3a%2f%2frgs-ld-demo.spinomenal.com%2fapi%2fInitClientUrl",
+        prefix: "2eulz8DD",
+        img: "majestic-wild-buffalo-dusk-till-dawn.jpg",
+        rating: 5,
+    },
+    {
+        title: "Tome of Madness",
+        source: "https://asccw.playngonetwork.com/Casino/IframedView?pid=2&gid=tomeofmadness&lang=en_US&practice=1&channel=desktop&div=flashobject&width=100%25&height=100%25&user=&password=&ctx=&demo=2&brand=&lobby=&rccurrentsessiontime=0&rcintervaltime=0&rcaccounthistoryurl=&rccontinueurl=&rcexiturl=&rchistoryurlmode=&autoplaylimits=0&autoplayreset=0&callback=flashCallback&rcmga=&resourcelevel=0&hasjackpots=False&country=&pauseplay=&playlimit=&selftest=&sessiontime=&coreweburl=https://asccw.playngonetwork.com/&showpoweredby=True",
+        prefix: "1eulvjl2",
+        img: "Tome-of-Madness.jpg",
+        rating: 4,
+    },
+    {
+        title: "Rich Piggies Bonus",
+        source: "https://www.netgamenv.com/netgame_games/db.php?game=rich-piggies-bonus-combo",
+        prefix: "VeulbBkw",
+        img: "Rich-Piggies-Bonus-Combo.jpg",
+        rating: 5,
+    },
+];
+
+// var title = [
+//   "24",
+//   "4 of a King",
+//   "5 reel drive",
+//   "5xmagic",
+//   "A Christmas Carol",
+//   "Ace of spades",
+//   "Crystal clans",
+//   "Platoon wild",
+//   "Royal mint megaways",
+//   "Sic bo",
+//   "Spinfinity man",
+//   "Stacks o gold",
+//   "Super diamond wild",
+//   "The wiz",
+//   "Joker King",
+//   "Queen’s day tilt",
+//   "Xmas Joker",
+//   "Wild North",
+// ];
+//
+// var links = [
+//   "4M82Hvi",
+//   "gM82ZHk",
+//   "VM82NJO",
+//   "HM821SP",
+//   "hM822Lt",
+//   "9M8239W",
+//   "VM827lI",
+//   "HM826wm",
+//   "tM89qOd",
+//   "gM89rHk",
+//   "oM89yxC",
+//   "FM89ost",
+//   "dM89abK",
+//   "5M89dCp",
+//   "xM89gCf",
+//   "aM89jRC",
+//   "KM89lyc",
+// ];
+
+const max = games.length - 1,
+    min = 0,
+    bstar = "★",
+    wstar = "☆";
+
+let html_temp = "";
+
+let count = 0;
+let i = startRange;
+
+while (count < size && i < games.length) {
+    if (!filtered.includes(i)) {
+        var game_stars = bstar.repeat(games[i].rating),
+            game_name = games[i].title,
+            game_image = `/images/${games[i].img}`,
+            game_url = "game.php?g=" + games[i].prefix;
+
+        html_temp += `
+	
+
+<div class="col-lg-6 col-xl-4 wow fadeInUp" data-wow-delay="0.2s">
+                        <div class="blog-item">
+                            <div class="blog-img">
+                                <img src="${game_image}" class="img-fluid rounded-top w-100" alt="Top slot games to try">
+                            </div>
+                            <div class="blog-content rounded-bottom p-4">
+                                <a href="${game_url}" class="h4 d-inline-block mb-3">${game_name}</a>
+                                <p style="color:gold; font-size:25px;" >${game_stars}</p>
+                                <a href="${game_url}" class="fw-bold text-secondary">PLAY NOW <i class="fa fa-angle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+    `;
+
+        count++;
+    }
+    i++;
+}
+
+document.getElementById("games_selector").innerHTML = html_temp;
